985cd552f86825bd8019f6147d6557155a18a6d2
xiangyangyang <greylalalalu11@gmail.com>
2020-02-11 16:31:59 +0900

    =_=

--- app/src/main/java/com/chai/xiangyang/stickerheader/view/StickerActivity.kt
+++ app/src/main/java/com/chai/xiangyang/stickerheader/view/StickerActivity.kt
@@ -14,10 +14,7 @@ class StickerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sticker)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
     }
 
 }
